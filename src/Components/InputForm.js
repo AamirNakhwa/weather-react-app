b37c90990d102c200153bef37571f33cbaa9aad8
Aamir Nakhwa <aamirnakhwa@yahoo.com>
2023-08-31 10:34:59 +0500

    Error handling and fixes

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -7,34 +7,42 @@ import WeatherCard from './WeatherCard';
 // import { Carousel } from 'react-responsive-carousel';
 
 function InputForm(props) {
-    console.log('InputForm');
     let [weatherData, setWeatherData] = useState([]);
+    let [errorMessage, setErrorMessage] = useState(null);
 
     const cityInput = useRef(null);
-    const [isLit, setLit] = useState(false);
+    //const [isLit, setLit] = useState(false);
 
     const submitHandler = async (event) => {
         event.preventDefault();
         const city = cityInput.current.value;
         if (!city) {
-            alert('Please enter city...');
+            setErrorMessage('Please enter city...');
             return;
         }
-        
-        const url = `https://api.weatherapi.com/v1/current.json?key=505cd211ccfb414b811153617230707&q=${city}&aqi=no`;
-        const response = await axios.get(url);
+        try {
+            const url = `https://api.weatherapi.com/v1/current.json?key=505cd211ccfb414b811153617230707&q=${city}&aqi=no`;
+            const response = await axios.get(url);
 
-        cityInput.current.value = '';
-        setLit(!isLit)
+            cityInput.current.value = '';
+            //event.target.reset();
 
-        //weatherData.push(response.data);
-        weatherData = [response.data, ...weatherData]
-        setWeatherData(weatherData);
+            //setLit(!isLit)
+
+            //weatherData.push(response.data);
+            weatherData = [response.data, ...weatherData]
+            setWeatherData(weatherData);
+            setErrorMessage(null);
+        }
+        catch (err) {
+            console.log(err);
+            setErrorMessage('Something went wrong, kindly make sure you entered the correct city.');
+        }
     }
 
     return (
         <>
-            <h1>Hello {isLit ? "True" : "False"}</h1>
+            {/* <h1>Hello {isLit ? "True" : "False"}</h1> */}
             <form className="input_form" onSubmit={submitHandler}>
                 <div className="input-group">
                     <input type="text" className="form-control" name="city" placeholder="Enter city here"
@@ -45,14 +53,15 @@ function InputForm(props) {
                     </span>
                 </div>
             </form>
-
-            <div className="location-results-container">
-                {
-                    weatherData.reverse().map((item, index) => 
-                        <WeatherCard key={index} data={item} />
-                    )
-                }
-            </div>
+            {(errorMessage !== null) ? <div className="error-container"><span>{errorMessage}</span></div> :
+                <div className="location-results-container">
+                    {
+                        weatherData.map((item, index) =>
+                            <WeatherCard key={index} data={item} />
+                        )
+                    }
+                </div>
+            }
         </>
     );
 }
